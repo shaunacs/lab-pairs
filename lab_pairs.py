986d47bf94ff0026b03bda948ec2f20bc3868ada
Shauna Saunders <shaunacsaunders@outlook.com>
2021-08-26 10:44:00 -0400

    Create make lab pairs function

--- lab_pairs.py
+++ lab_pairs.py
@@ -2,27 +2,30 @@ from random import choice
 
 all_students = []
 
-present_students = []
+present_students = ['Ashley', 'Shauna', 'Kat', "Anjelica", 'Pat']
 
 rooms = ["Malala A", "Malala B", "Malala C", "Ruth A", "Ruth B", "Ruth C"]
 
-pairings = []
 
-while present_students != []:
-    if len(present_students) > 1:
-        student_to_pair = choice(present_students)
-        present_students.remove(student_to_pair)
-        pair = choice(present_students)
-        present_students.remove(pair)
-        pairings.append([student_to_pair, pair])
-    else:
-        last_pair = present_students[0]
-        pairings[-1].append(last_pair)
-        present_students.remove(last_pair)
+def make_lab_pairs(students, rooms):
+    """Makes lab pairs from list of students and list of rooms"""
 
-# test push
-for idx, pair in enumerate(pairings):
-    if len(pair) == 2:
-        print(f'{rooms[idx]} -- {pair[0]} & {pair[1]}')
-    elif len(pair) == 3:
-        print(f'{rooms[idx]} -- {pair[0]}, {pair[1]} & {pair[2]}')
+    pairings = []
+
+    while students != []:
+        if len(students) > 1:
+            student_to_pair = choice(students)
+            students.remove(student_to_pair)
+            pair = choice(students)
+            students.remove(pair)
+            pairings.append([student_to_pair, pair])
+        else:
+            last_pair = students[0]
+            pairings[-1].append(last_pair)
+            students.remove(last_pair)
+
+    for idx, pair in enumerate(pairings):
+        if len(pair) == 2:
+            print(f'{rooms[idx]} -- {pair[0]} & {pair[1]}')
+        elif len(pair) == 3:
+            print(f'{rooms[idx]} -- {pair[0]}, {pair[1]} & {pair[2]}')
